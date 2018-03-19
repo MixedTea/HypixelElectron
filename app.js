eda6757d06252d64c3eec4b0695f1ef34a81b97a
MixedTea <nxtyofficially@gmail.com>
2018-03-19 13:33:51 -0600

    Setup #2

---
+++ app.js
@@ -0,0 +1,28 @@
+const https = require("https");
+const {BrowserWindow, app, ipcMain, Menu} = require("electron");
+const path = require("path");
+const url = require("url");
+
+let win;
+
+function createWindow(){
+    win = new BrowserWindow({minWidth: 1280, minHeight: 720});
+    
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    win.on('closed', () => {
+        win = null;
+    });
+    win.focus();
+    Menu.setApplicationMenu(null);
+}
+
+app.on('ready', createWindow);
+app.on('activate', () => {
+    if(win === null){
+        createWindow();
+    }
+});
